b8a9a95b7122f1674a9ccb536aacb21fdcc1cb26
ChaconMoon <chaconmooncarlos@gmail.com>
2022-11-09 11:36:22 +0100

    Finished

--- app/src/main/java/com/chaconmoon/myapplication/Activity1.java
+++ app/src/main/java/com/chaconmoon/myapplication/Activity1.java
@@ -2,13 +2,58 @@ package com.chaconmoon.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Activity1 extends AppCompatActivity {
 
+    private String mNombre;
+    private String mApellidos;
+    private String mCurso;
+    private String toastNombre;
+    private String toastApellidos;
+    private String toastCurso;
+    private Button nVisualizarButton;
+    private Intent mIntent;
+    private final String NAME_KEY = "Nombre";
+    private final String SURNAME_KEY = "Apellidos";
+    private final String GRADE_KEY = "Curso";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nVisualizarButton = findViewById(R.id.visualizar);
+        nVisualizarButton.setOnClickListener(view -> {
+           mNombre=verNombre();
+           toastNombre = "El Nombre es: "+ mNombre;
+           mApellidos=verApellidos();
+           toastApellidos = "Los Apellidos son: "+ mApellidos;
+           mCurso=verCurso();
+           toastCurso = "El curso es: "+mCurso;
+            //Toast.makeText(this,toastNombre,Toast.LENGTH_SHORT).show();
+            //Toast.makeText(this,toastApellidos,Toast.LENGTH_SHORT).show();
+            //Toast.makeText(this,toastCurso,Toast.LENGTH_SHORT).show();
+            mIntent = new Intent(Activity1.this,Activity2.class);
+            mIntent.putExtra(NAME_KEY,mNombre);
+            mIntent.putExtra(SURNAME_KEY,mApellidos);
+            mIntent.putExtra(GRADE_KEY, mCurso);
+            startActivity(mIntent);
+        });
+
+    }
+    private String verNombre(){
+        EditText nombre = (EditText) findViewById(R.id.Nombre);
+        return nombre.getText().toString();
+    }
+    private String verApellidos(){
+        EditText apellidos = (EditText) findViewById(R.id.apellidos);
+        return apellidos.getText().toString();
+    }
+    private String verCurso(){
+        EditText curso = (EditText) findViewById(R.id.curso);
+        return curso.getText().toString();
     }
 }
--- app/src/main/java/com/chaconmoon/myapplication/Activity2.java
+++ app/src/main/java/com/chaconmoon/myapplication/Activity2.java
@@ -1,4 +1,37 @@
 package com.chaconmoon.myapplication;
 
-public class Activity2 {
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity2 extends AppCompatActivity {
+    TextView nombre;
+    TextView apellidos;
+    TextView curso;
+    String textNombre;
+    String textApellidos;
+    String textCurso;
+    private final String NAME_KEY = "Nombre";
+    private final String SURNAME_KEY = "Apellidos";
+    private final String GRADE_KEY = "Curso";
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+
+        nombre = findViewById(R.id.TextNombre);
+        textNombre = getIntent().getStringExtra(NAME_KEY);
+        nombre.setText(textNombre);
+
+        apellidos = findViewById(R.id.Textapellidos);
+        textApellidos = getIntent().getStringExtra(SURNAME_KEY);
+        apellidos.setText(textApellidos);
+
+        curso = findViewById(R.id.Textcurso);
+        textCurso = getIntent().getStringExtra(GRADE_KEY);
+        curso.setText(textCurso);
+    }
 }
